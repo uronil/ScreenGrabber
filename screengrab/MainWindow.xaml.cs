7570058b051c2f5f30546d80a08e99f31fdc082a
ununoctium1187 <ununoctium1187@googlemail.com>
2017-08-01 18:28:46 +0300

    Find optimal decide for global hook, working fine

--- screengrab/MainWindow.xaml.cs
+++ screengrab/MainWindow.xaml.cs
@@ -19,81 +19,34 @@ namespace screengrab
 {
     public partial class MainWindow : Window
     {
-        List<Key> _pressedKeys;
-        //private LowLevelKeyboardListener _listener;
-
+        // Variables for keyboard
+        List<Key> pressedKeys; // Keyboard stack
         KeyboardListener KListener = new KeyboardListener();
 
         public MainWindow() {
             InitializeComponent();
-            //HotKey _hotKey = new HotKey(Key.C, KeyModifier.Ctrl | KeyModifier.Shift , OnHotKeyHandler);
 
-            _pressedKeys = new List<Key>();
-
-            //KBDHook.KeyDown += new KBDHook.HookKeyPress(KBDHook_KeyDown);
-            //KBDHook.KeyUp += new KBDHook.HookKeyPress(KBDHook_KeyUp);
-            //KBDHook.LocalHook = false;
-            //KBDHook.InstallHook();
-            //this.Closed += (s, e) => {
-            //    KBDHook.UnInstallHook();
-            //};
-
+            // Keyboard initialization
+            pressedKeys = new List<Key>();
             KListener.KeyDown += new RawKeyEventHandler(KListener_KeyDown);
             KListener.KeyUp += new RawKeyEventHandler(KListener_KeyUp);
-
-            //_listener = new LowLevelKeyboardListener();
-            //_listener.OnKeyPressed += _listener_OnKeyPressed;
-            //_listener.OnKeyPressed += _listener_OnKeyUp;
-
-            //_listener.HookKeyboard();
         }
 
         void KListener_KeyDown(object sender, RawKeyEventArgs e) {
-            if (!_pressedKeys.Contains(e.Key))
-                _pressedKeys.Add(e.Key);
+            if (!pressedKeys.Contains(e.Key))
+                pressedKeys.Add(e.Key);
             link_website.Content = e.Key.ToString();
-            link_profile.Content = string.Join<Key>(" + ", _pressedKeys);
+            link_profile.Content = string.Join<Key>(" + ", pressedKeys);
         }
 
         void KListener_KeyUp(object sender, RawKeyEventArgs e) {
-            _pressedKeys.Remove(e.Key);
-            link_profile.Content = string.Join<Key>(" + ", _pressedKeys);
+            pressedKeys.Remove(e.Key);
+            link_profile.Content = string.Join<Key>(" + ", pressedKeys);
         }
-
-        //void _listener_OnKeyUp(object sender, KeyPressedArgs e) {
-        //    link_profile.Content = e.KeyPressed.ToString();
-
-        //}
-
-        //void _listener_OnKeyPressed(object sender, KeyPressedArgs e) {
-        //    if (!_pressedKeys.Contains(e.Keys))
-        //                _pressedKeys.Add(e.Keys);
-        //           link_profile.Content = string.Join<Key>(" + ", _pressedKeys);
-        //        _listener.UnHookKeyboard();
-        //    _listener.HookKeyboard();
-        //}
-
-        void KBDHook_KeyUp(LLKHEventArgs e) {
-            _pressedKeys.Remove(e.Keys);
-            link_profile.Content = string.Join<Key>(" + ", _pressedKeys);
-        }
-
-        void KBDHook_KeyDown(LLKHEventArgs e) {
-            if (!_pressedKeys.Contains(e.Keys))
-                _pressedKeys.Add(e.Keys);
-            link_website.Content = e.Keys.ToString();
-            link_profile.Content = string.Join<Key>(" + ", _pressedKeys);
-        }
-
-
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            MessageBox.Show("lloll");
-        }
-
-        //private void OnHotKeyHandler(HotKey hotKey) {
-        //    //link_profile.Content = hotKey.Key.ToString() + " " + hotKey.KeyModifiers.ToString();
-        //}
         
-
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            
+        }
+        
     }
 }
