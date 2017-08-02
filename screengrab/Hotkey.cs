f8e06ed00d85be0fbe536d8e5a23e02cd400ec46
ununoctium1187 <ununoctium1187@googlemail.com>
2017-08-02 23:50:09 +0300

    Hotkey class update

--- screengrab/Hotkey.cs
+++ screengrab/Hotkey.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace screengrab
 {
     class Hotkey
     {
+        public string name;
+        private List<Key> hotkey;
+
+        public Hotkey(string name, List<Key> hotkey) {
+            this.name = name;
+            this.hotkey = hotkey;
+        }
+        
+        public override string ToString() {
+            return string.Join<Key>(" + ", hotkey);
+        }
     }
 }
