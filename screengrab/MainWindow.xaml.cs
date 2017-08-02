c73c719654f2406244c193a166426b0342427627
ununoctium1187 <ununoctium1187@googlemail.com>
2017-08-02 23:39:38 +0300

    Create Hotkey class

--- screengrab/MainWindow.xaml.cs
+++ screengrab/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
+using System.Linq;
+using System.Collections;
 
 namespace screengrab
 {
@@ -10,7 +12,6 @@ namespace screengrab
         // Variables for keyboard
         List<Key> pressedKeys;
         KeyboardListener KListener = new KeyboardListener();
-
         public MainWindow() {
             InitializeComponent();
 
@@ -18,23 +19,22 @@ namespace screengrab
             pressedKeys = new List<Key>();
             KListener.KeyDown += new RawKeyEventHandler(KListener_KeyDown);
             KListener.KeyUp += new RawKeyEventHandler(KListener_KeyUp);
+            
         }
 
         void KListener_KeyDown(object sender, RawKeyEventArgs e) {
             if (!pressedKeys.Contains(e.Key))
                 pressedKeys.Add(e.Key);
-            link_website.Content = e.Key.ToString();
             link_profile.Content = string.Join<Key>(" + ", pressedKeys);
         }
 
         void KListener_KeyUp(object sender, RawKeyEventArgs e) {
             pressedKeys.Remove(e.Key);
-            link_profile.Content = string.Join<Key>(" + ", pressedKeys);
         }
-        
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            
+
+        public void Button_Click(object sender, RoutedEventArgs e) {
+
         }
-        
+
     }
 }
