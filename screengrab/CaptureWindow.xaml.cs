413675cbd144007086957c0160fc10e927e0e53b
ununoctium1187 <ununoctium1187@googlemail.com>
2017-08-04 11:58:59 +0300

    Remove all doing with image, need more brains

--- screengrab/CaptureWindow.xaml.cs
+++ screengrab/CaptureWindow.xaml.cs
@@ -8,13 +8,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Drawing;
-using System.IO;
-using System.Drawing.Imaging;
 
 namespace screengrab
 {
@@ -41,22 +34,7 @@ namespace screengrab
 
             this.Top = 0;
             this.Left = 0;
-
-            Bitmap bmp = new Bitmap((int)screenWidth, (int)screenHeight);
-            Graphics g = Graphics.FromImage(bmp);
-            g.CopyFromScreen((int)screenLeft, (int)screenTop, 0, 0, bmp.Size);
-            using (MemoryStream memory = new MemoryStream()) {
-                bmp.Save(memory, ImageFormat.Png);
-                memory.Position = 0;
-                BitmapImage bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-
-                image.Source = bitmapImage;
-            }
+            
         }
-
     }
 }
